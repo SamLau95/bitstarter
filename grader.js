3b04ab9fb0f2a2bb42733023da4a186e292eaa96
Sam Lau <samlau95@gmail.com>
2013-07-15 20:59:57 +0000

    HW3 complete

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -40,12 +41,16 @@ var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
 
+var cheerioHtml = function(html) {
+    return cheerio.load(html);
+};
+
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function(htmlfile, checksfile, cheerioFunc) {
+    $ = cheerioFunc(htmlfile);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -61,14 +66,28 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var htmlCheck = function(url, checks) {
+    rest.get(url).on('success', function(result) {})
+};
+
+var checkAndOutput = function(file, checksFile, cheerioFunc) {
+    var checkJson = checkHtmlFile(file, checksFile, cheerioFunc);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+}
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Url for app')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if(program.url)
+        rest.get(program.url).on('success', function(result) {
+            checkAndOutput(result, program.checks, cheerioHtml);
+        });
+    else
+        checkAndOutput(program.file, program.checks, cheerioHtmlFile);    
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
